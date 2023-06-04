612d84ab70b763bc17cdaa504b48cfa91de21d07
ThaiBaor <ngphanthaibao203@gmail.com>
2023-06-04 23:20:29 +0700

    them crud vao list food

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import HomeScreen from "../screens/HomeScreen";
 import { FoodStackScreen } from "./NativeStack";
 import { CartStackScreen } from "./CartStack";
+import { AuthStackScreen } from "./AuthStack";
 
 const Tab = createBottomTabNavigator();
 const Tabs = () => {
@@ -34,17 +35,14 @@ const Tabs = () => {
                 tabBarIcon: ({ focused }) => !focused ? (<Image source={require('../assets/icons/inactivesearch.png')} style={{ width: 35, height: 35, position: 'absolute', bottom: 22 }}></Image>) : (<Image source={require('../assets/icons/activesearch.png')} style={{ width: 35, height: 35, position: 'absolute', bottom: 22 }}></Image>)
             }
             }></Tab.Screen>
-
-
-            <Tab.Screen name="Account" component={FoodStackScreen} options={{
-                tabBarIcon: ({ focused }) => !focused ? (<Image source={require('../assets/icons/inactiveuser.png')} style={{ width: 25, height: 25 }}></Image>) : (<Image source={require('../assets/icons/activeuser.png')} style={{ width: 25, height: 25 }}></Image>)
-            }
-            }></Tab.Screen>
-            <Tab.Screen  name="Cart" component={CartStackScreen} options={{
+            <Tab.Screen name="Cart" component={CartStackScreen} options={{
                 tabBarIcon: ({ focused }) => !focused ? (<Image source={require('../assets/icons/inactivecart.png')} style={{ width: 25, height: 25 }}></Image>) : (<Image source={require('../assets/icons/activecart.png')} style={{ width: 25, height: 25 }}></Image>)
             }
             }></Tab.Screen>
-
+            <Tab.Screen name="Account" component={AuthStackScreen} options={{
+                tabBarIcon: ({ focused }) => !focused ? (<Image source={require('../assets/icons/inactiveuser.png')} style={{ width: 25, height: 25 }}></Image>) : (<Image source={require('../assets/icons/activeuser.png')} style={{ width: 25, height: 25 }}></Image>)
+            }
+            }></Tab.Screen>
         </Tab.Navigator>
     )
 }
--- components/NativeStack.js
+++ components/NativeStack.js
@@ -4,16 +4,24 @@ import CategoryFoodScreen from "../screens/CategoryFoodScreen";
 import ListFoodScreen from "../screens/ListFoodScreen";
 import CURDFood from "./CRUDFood";
 import DetailFood from "./DetailFood"
-import CartScreen from "../screens/CartScreen";
+import { TouchableOpacity, Text } from "react-native";
 
 
 const FoodStack = createNativeStackNavigator();
-function FoodStackScreen(){
+function FoodStackScreen({navigation}){
     return(
         <FoodStack.Navigator>
             <FoodStack.Screen name="Category Food List" component={CategoryFoodScreen}/>
-            <FoodStack.Screen name="Food List" component={ListFoodScreen}></FoodStack.Screen>
-            <FoodStack.Screen name="CURD Food" component={CURDFood}></FoodStack.Screen>
+            <FoodStack.Screen name="Food List" options={
+                {
+                    headerRight: ()=>(
+                        <TouchableOpacity onPress={()=>navigation.navigate('CRUD Food')}>
+                            <Text>CRUD</Text>
+                        </TouchableOpacity>
+                    )                       
+                }
+            } component={ListFoodScreen}></FoodStack.Screen>
+            <FoodStack.Screen name="CRUD Food" component={CURDFood}></FoodStack.Screen>
             <FoodStack.Screen name="Detail Food" component={DetailFood}></FoodStack.Screen>
         </FoodStack.Navigator>
     )
